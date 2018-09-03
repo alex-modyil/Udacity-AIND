f2ab1cdff294a6dad8760efd3f01fbac751be31f
axm155231 <axm155231@utdallas.edu>
2018-09-03 00:29:56 -0500

    add aind projects

---
+++ 3. Build a Game Playing Agent/my_custom_player.py
@@ -0,0 +1,84 @@
+
+from sample_players import DataPlayer
+import random
+
+class CustomPlayer(DataPlayer):
+    """ Implement your own agent to play knight's Isolation
+
+    The get_action() method is the only *required* method. You can modify
+    the interface for get_action by adding named parameters with default
+    values, but the function MUST remain compatible with the default
+    interface.
+
+    **********************************************************************
+    NOTES:
+    - You should **ONLY** call methods defined on your agent class during
+      search; do **NOT** add or call functions outside the player class.
+      The isolation library wraps each method of this class to interrupt
+      search when the time limit expires, but the wrapper only affects
+      methods defined on this class.
+
+    - The test cases will NOT be run on a machine with GPU access, nor be
+      suitable for using any other machine learning techniques.
+    **********************************************************************
+    """
+    def get_action(self, state):
+        """ Employ an adversarial search technique to choose an action
+        available in the current state calls self.queue.put(ACTION) at least
+
+        This method must call self.queue.put(ACTION) at least once, and may
+        call it as many times as you want; the caller is responsible for
+        cutting off the function after the search time limit has expired. 
+
+        See RandomPlayer and GreedyPlayer in sample_players for more examples.
+
+        **********************************************************************
+        NOTE: 
+        - The caller is responsible for cutting off search, so calling
+          get_action() from your own code will create an infinite loop!
+          Refer to (and use!) the Isolation.play() function to run games.
+        **********************************************************************
+        """
+        # TODO: Replace the example implementation below with your own search
+        #       method by combining techniques from lecture
+        #
+        # EXAMPLE: choose a random move without any search--this function MUST
+        #          call self.queue.put(ACTION) at least once before time expires
+        #          (the timer is automatically managed for you)
+        if state.ply_count < 2:
+            self.queue.put(random.choice(state.actions()))
+        else:
+            self.queue.put(self.alphabeta_search(state, 3))
+
+    def alphabeta_search(self, state, depth):    
+
+        def min_value(state, depth, alpha, beta):
+            if state.terminal_test(): return state.utility(self.player_id)
+            if depth <= 0 : return self.score(state)
+            value = float("inf")
+            for action in state.actions():
+                value = min(value, max_value(state.result(action), depth - 1, alpha, beta))
+                if value <= alpha: return value
+                beta = min(beta, value)
+            return value 
+
+        def max_value(state, depth, alpha, beta):
+            if state.terminal_test(): return state.utility(self.player_id)
+            if depth <= 0 : return self.score(state)
+            value = float("-inf")
+            for action in state.actions():
+                value = max(value, min_value(state.result(action), depth - 1, alpha, beta))
+                if value >= beta: return value
+                alpha = max(alpha, value)
+            return value
+        
+        return max(state.actions(), key=lambda x: min_value(state.result(x), depth - 1, float('-inf'), float('inf')))       
+        
+    def score(self, state):
+        own_loc = state.locs[self.player_id]
+        opp_loc = state.locs[1 - self.player_id]
+        own_liberties = state.liberties(own_loc)     
+        opp_liberties = state.liberties(opp_loc)
+        future_own_liberties = sum(len(state.liberties(l)) for l in own_liberties)
+        future_opp_liberties = sum(len(state.liberties(l)) for l in opp_liberties)
+        return len(own_liberties) - 2 * len(opp_liberties) +  future_own_liberties - future_opp_liberties
